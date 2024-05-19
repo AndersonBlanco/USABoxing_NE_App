c9103c611f318b96037db876466a1871f1a9db4a
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2024-05-19 19:16:31 -0400

    update

--- usa_boxing_new/App.js
+++ usa_boxing_new/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import SVG from "./imgs/usaBoxingNE_logo.png"; 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>USA Boxing App</Text>
       <StatusBar style="auto" />
+      <Image source={SVG} height={100} width={100}/>
     </View>
   );
 }
